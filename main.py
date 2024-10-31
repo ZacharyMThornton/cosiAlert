715216694629c2ebc65f93e0741a890983c6e649
Zachary Thornton <thorntonzachary02@gmail.com>
2024-10-31 13:19:15 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from typing import Final
+import os
+import keyboard
+from dotenv import load_dotenv
+from discord import Intents, Client
+
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+async def send_message() -> None:
+    try:
+        channel = client.get_channel(1301381285784326157)
+        if channel:
+            response = "hello there how are you!"
+            await channel.send(response)
+        else:
+            print("channel not found")
+    except Exception as e:
+        print(e)
+
+@client.event
+async def on_ready():
+    print("bot is ready!")
+
+    def on_space_press():
+        client.loop.create_task(send_message())
+
+    keyboard.on_release_key("space", lambda _: on_space_press())
+    await send_message()
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == "__main__":
+    main()
